7a7b11d90afa5e65aa1609369d581f9c504855c7
Adam Zimmermann <adam@adamzimmermann.com>
2020-07-29 21:08:07 -0500

    Add @todo comment formatting check.

---
+++ src/Chromatic/Sniffs/Commenting/TodoFormatCommentSniff.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Parses and verifies comment language.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+namespace Chromatic\Sniffs\Commenting;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * Parses and verifies that comments use gender neutral language.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+class TodoFormatCommentSniff implements Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array<int|string>
+     */
+    public function register()
+    {
+        return [
+            T_COMMENT,
+            T_DOC_COMMENT_STRING,
+        ];
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the current token
+     *                                               in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens     = $phpcsFile->getTokens();
+        $expression = '/(?i)(?=(@to(-|)do))(?-i)(?!@todo)/m';
+        if ((bool) preg_match($expression, $tokens[$stackPtr]['content']) === true) {
+            $phpcsFile->addError('@todo comments should be in the "@todo" format.', $stackPtr, 'TodoFormat');
+        }
+
+    }//end process()
+
+
+}//end class
